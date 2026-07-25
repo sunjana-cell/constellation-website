d1dbbf3dab45e94f5542455b6284f26dc1952dfe
sunjana-cell <sunjana99100@gmail.com>
2026-07-25 19:34:12 +0530

    My first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const signinbox= document.getElementById("signinbtn");
+
+if(signinbox){
+signinbox.addEventListener("click", function(){
+    const email = document.getElementById("email").value;
+     const username = document.getElementById("user").value;
+      const password = document.getElementById("password").value;
+
+      if(email===""||username===""||password===""){
+         alert("please fill all the details");
+      }else{
+        window.location.href="constellation.html";
+      }                                                                         
+});
+}
+
+function searchConstellation(){
+  console.log(search.value);
+}
+const search=document.getElementById("searchbtn");
+
+if(search){
+search.addEventListener("keydown", function(event){
+  if(event.key === "Enter"){
+   console.log(search.value);
+  }
+});
+}
+
+const searchIcon=document.querySelector(".fa-magnifying-glass");
+
+if(searchIcon){
+searchIcon.addEventListener("click", function(){
+   console.log(search.value);
+});
+}
+
+const contact=document.getElementById("contactme");
+const foot=document.getElementById("contactji");
+
+contact.addEventListener("click",function(){
+    foot.scrollIntoView({
+      behavior:"smooth"
+    });
+})
