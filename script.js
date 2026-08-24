7c9128bf0ae061815c70470154a7f4ead1393f40
sunjana-cell <sunjana99100@gmail.com>
2026-08-24 19:52:51 +0530

    updated js

--- script.js
+++ script.js
@@ -38,21 +38,50 @@ searchIcon.addEventListener("click", function(){
 const contact=document.getElementById("contactme");
 const foot=document.getElementById("contactji");
 
-contact.addEventListener("click",function(){
-    foot.scrollIntoView({
-      behavior:"smooth"
-    });
-});
+if (contact && foot) {
+  contact.addEventListener("click",function(){
+      foot.scrollIntoView({
+        behavior:"smooth"
+      });
+  });
+}
 
 const type=document.getElementById("types12");
 
- type.addEventListener("click", function() {
-  const types12= this.value;
-  if (types12) {
-    const all = document.getElementById("polarhead");
-    if (all) {
-      all.scrollIntoView({ 
-        behavior: 'smooth' });
+if (type) {
+  type.addEventListener("click", function() {
+    const types12= this.value;
+    if (types12) {
+      const all = document.getElementById("polarhead");
+      if (all) {
+        all.scrollIntoView({ 
+          behavior: 'smooth' });
+      }
     }
-  }
-});
+  });
+}
+
+const fact = document.getElementById("one");
+const click = document.getElementById("clicki");
+
+const facts = [
+  "Aquarius is one of the oldest recognized constellations, positioned in a region of the sky often called The Sea because it is surrounded by other water-related constellations like Cetus (the whale), Pisces (the fish), and Eridanus (the river).",
+  "orion is one of the most easily recognizable constellation",
+  "scorpius contains the bright star Antras",
+  "The constellation leo represents a lion",
+  "Ursa major contains the famous Big Dipper",
+];
+
+let currentFact = 0;
+
+if (click && fact) {
+  click.addEventListener("click",function(){
+      currentFact++;
+
+      if(currentFact >= facts.length){
+        currentFact = 0;
+      }
+
+      fact.textContent = facts[currentFact];
+  });
+}
